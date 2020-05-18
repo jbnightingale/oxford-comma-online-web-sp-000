d2a82cf8270417cf9837bcc7b9bc4053da7d0111
Brandon Nightingale <brandon@jbnight.com>
2020-05-18 21:41:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.size <= 2
     array.join(" and ")
   elsif array.size >= 3
-    array_size = array.size
+    second_to_last = array.size - 1
     array.join(", ")
-    array[array_size - 1] << "and "
+    array.insert(second_to_last, " and ")
+    #add " and " at the possition 1 minus the array length
   end
 end
