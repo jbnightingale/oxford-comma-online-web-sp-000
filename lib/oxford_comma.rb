8e0e2fefc63f09e14c928ae040d72d45cb3fd601
Brandon Nightingale <brandon@jbnight.com>
2020-05-18 21:37:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.size <= 2
     array.join(" and ")
-  elsif array.size == 3
+  elsif array.size >= 3
+    array_size = array.size
     array.join(", ")
+    array[array_size - 1] << "and "
   end
 end
